685cd5720458d6bd8c74bbbd8159c11d4811d4c2
Maksym Ostapchuk <gadget.add@gmail.com>
2023-05-22 18:49:14 +0300

    add camera

--- src/screens/CreatePostsScreen.js
+++ src/screens/CreatePostsScreen.js
@@ -1,18 +1,115 @@
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
 
 export default function CreatePostsScreen() {
+
+    const [hasPermission, setHasPermission] = useState(null);
+    const [cameraRef, setCameraRef] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            await MediaLibrary.requestPermissionsAsync();
+
+            setHasPermission(status === "granted");
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
         <View style={styles.container}>
+            <Camera
+                style={styles.camera}
+                type={type}
+                pictureSize="1280x960"
+                ref={setCameraRef}
+            >
+                <View style={styles.photoView}>
+                    <TouchableOpacity
+                        style={styles.flipContainer}
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                            );
+                        }}
+                    >
+                        <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+                            {" "}
+                            Flip{" "}
+                        </Text>
+                    </TouchableOpacity >
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={async () => {
+                            if (cameraRef) {
+                                const { uri } = await cameraRef.takePictureAsync();
+                                await MediaLibrary.createAssetAsync(uri);
+                                const sizes = await cameraRef.getAvailablePictureSizesAsync("4:3");
+                                console.log(sizes);
+                            }
+                        }}
+                    >
+                        <View style={styles.takePhotoOut}>
+                            <View style={styles.takePhotoInner}></View>
+                        </View>
+                    </TouchableOpacity>
+                </View >
+            </Camera >
             <Text style={styles.text}>CreatePostsScreen</Text>
-        </View>
+        </View >
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    container: { flex: 1 },
+    camera: {
+        aspectRatio: 4 / 3,
+        resizeMode: 'center',
+    },
+    photoView: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: "transparent",
+        justifyContent: "flex-end",
+    },
+
+    flipContainer: {
+        flex: 0.1,
+        alignSelf: "flex-end",
+    },
+
+    button: { alignSelf: "center" },
+
+    takePhotoOut: {
+        borderWidth: 2,
+        borderColor: "white",
+        height: 50,
+        width: 50,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 50,
+    },
+
+    takePhotoInner: {
+        borderWidth: 2,
+        borderColor: "white",
+        height: 40,
+        width: 40,
+        backgroundColor: "white",
+        borderRadius: 50,
     },
     text: {
         fontSize: 36,
